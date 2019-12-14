f3507d6fca5cc79aff1ca54a6895cc633d2caece
satoru103 <58040354+satoru103@users.noreply.github.com>
2019-12-14 15:51:42 +0900

    3 kommit

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "messages#index"
-  resources :messages, only: [:edit,:update]
+  resources :users, only: [:edit,:update]
   resources :groups, only:[:new,:create,:edit,:update]
 
 end
