6470fa80318f7a7c3c3cc0d2f28adf0cdcdd3cbc
satoru103 <58040354+satoru103@users.noreply.github.com>
2020-01-21 22:12:54 +0900

    2deploy

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -4,7 +4,8 @@ app_path = File.expand_path('../../../', __FILE__)
 #アプリケーションサーバの性能を決定する
 worker_processes 1
 
-#アプリケーションの設置されているディレクトリを指定
+working_directory "#{app_path}/current"
+
 listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
 pid "#{app_path}/shared/tmp/pids/unicorn.pid"
 stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
